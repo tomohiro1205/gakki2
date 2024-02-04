d4764b548257cea8a079e168f1e1ae82f94e489e
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-04 22:38:50 +0900

    Initial Commit

---
+++ gakki/gakkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gakkiApp.swift
+//  gakki
+//
+//  Created by 木村朋広 on 2024/02/04.
+//
+
+import SwiftUI
+
+@main
+struct gakkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
